e259b0d9905a62f1fd68dd91c978ec188d7a9a25
Ljulya1981 <uulia9684@gmail.com>
2025-05-21 09:51:20 +0500

    Update PageAuth.py

--- lesson_10/Pages/PageAuth.py
+++ lesson_10/Pages/PageAuth.py
@@ -19,27 +19,24 @@ class PageAuth:
         self.driver.get("https://www.saucedemo.com/")
 
     @allure.step("Введение имени пользователя на странице авторизации")
-    def user_name(self, driver):
+    def user_name(self):
         """
         Находит поле "Username" и вводит имя пользователя
-        :param driver: WebDriver — объект драйвера Selenium
         """
         username = self.driver.find_element(By.CSS_SELECTOR, "#user-name")
         username.send_keys("standard_user")
 
     @allure.step("Введение пароля на странице авторизации")
-    def password(self, driver):
+    def password(self):
         """
         Находит поле "Password" и вводит пароль
-        :param driver: WebDriver — объект драйвера Selenium
         """
         password = self.driver.find_element(By.CSS_SELECTOR, "#password")
         password.send_keys("secret_sauce")
 
     @allure.step("Нажатие кнопки 'Login'")
-    def click(self, driver):
+    def click(self):
         """
         Находит кнопку "Login" и нажимает на нее
-        :param driver: WebDriver — объект драйвера Selenium
         """
         self.driver.find_element(By.CSS_SELECTOR, "#login-button").click()
